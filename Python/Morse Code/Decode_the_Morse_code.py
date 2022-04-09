dd342bafc972be17783c09b3c5aa16d18b75fc73
Hêlēl <34353323+daminfer@users.noreply.github.com>
2022-04-09 13:27:46 +0200

    Add files via upload

---
+++ Python/Morse Code/Decode_the_Morse_code.py
@@ -0,0 +1,15 @@
+""" Problème Rank: 6 Kyu
+
+Dans ce kata, vous devez écrire un simple décodeur de code Morse.
+Bien que le code Morse soit désormais largement remplacé par les canaux de communication vocale et
+de données numériques, il est toujours utilisé dans certaines applications à travers le monde.
+
+
+
+"""
+# MORSE_CODE est stocker sur codewar ...
+
+def decode_morse(morseCode):
+    return ' '.join(''.join(MORSE_CODE[letter] for letter in word.split(' ')) for word in morseCode.strip().split('   '))
+
+print( decode_morse('.... . -.--   .--- ..- -.. .') )
