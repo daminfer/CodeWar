45a47a5799cb8b1e82d6d97fd0a4a6d5d745c818
Hêlēl <34353323+daminfer@users.noreply.github.com>
2022-05-03 23:39:31 +0200

    Add files via upload

---
+++ Langage C/Beginner_Series_4_Cockroach.c
@@ -0,0 +1,23 @@
+/* Problème Rank: 8 Kyu
+
+The cockroach is one of the fastest insects. 
+Write a function which takes its speed in km per hour and returns it in cm per second, 
+rounded down to the integer (= floored).
+
+For example:
+1.08 --> 30
+
+Note! The input is a Real number (actual type is language dependent) and is >= 0. 
+The result should be an Integer.
+
+*/
+
+#include <stdio.h>
+
+int cockroach_speed(double s){
+    return s / 0.036 ;
+}
+
+int main(){
+    printf("%d", cockroach_speed(1.09) );
+}
