9e1975da69935a5474316f396101cecaf822dd62
Hêlēl <34353323+daminfer@users.noreply.github.com>
2022-05-23 10:29:38 +0200

    Add files via upload

---
+++ Python/String_repeat.py
@@ -0,0 +1,11 @@
+# Problème Rank: 8 Kyu
+
+"""
+Write a function called repeatStr which repeats the given string string exactly n times.
+
+repeatStr(6, "I") // "IIIIII"
+repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
+"""
+
+def repeat_str(repeat, string):
+    return string * repeat
