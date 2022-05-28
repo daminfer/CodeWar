0f2c4b0022579c5384756970c6170b06eb6d6fd8
Hêlēl <34353323+daminfer@users.noreply.github.com>
2022-05-28 11:52:13 +0200

    Add files via upload

---
+++ Python/Sort_Numbers.py
@@ -0,0 +1,17 @@
+# Problème Rank: 7 Kyu
+
+"""
+Finish the solution so that it sorts the passed in array of numbers. 
+If the function passes in an empty array or null/nil value then it should return an empty array.
+
+For example:
+
+solution([1,2,3,10,5]) # should return [1,2,3,5,10]
+solution(None) # should return []
+"""
+
+def solution(nums):
+    return sorted(nums) if nums else []
+
+def solution_2(nums):
+    return sorted(nums or [])
