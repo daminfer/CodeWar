cdb0430d6057299ec8d04306fef7843a0e28325f
Hêlēl <34353323+daminfer@users.noreply.github.com>
2022-05-29 10:45:09 +0200

    Add files via upload

---
+++ Python/Grasshopper_DebugsayHello.py
@@ -0,0 +1,16 @@
+# Problème Rank: 8 Kyu
+
+"""
+Debugging sayHello function
+The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+
+Example output:
+
+Hello, Mr. Spock
+"""
+
+def say_hello(name):
+    return "Hello, " + name
+
+def say_hello_2(name):
+    return f"Hello, {name}"
