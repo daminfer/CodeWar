fdb28b2de4925b2ef6e755bc93ab29fdfa14d0c7
Hêlēl <34353323+daminfer@users.noreply.github.com>
2022-06-06 23:06:25 +0200

    Add files via upload

---
+++ Java/Grasshopper_Fixateur_de_fonctions_de_base.java
@@ -0,0 +1,14 @@
+/* Problème Rank: 8 Kyu
+ * 
+ * Fix the function
+ * I created this function to add five to any number that was passed in to it and return the new value. It doesn't throw any errors but it returns the wrong number.
+ * Can you help me fix the function?
+ * 
+*/
+
+public class Grasshopper_Fixateur_de_fonctions_de_base {
+    public static int addFive(int num) {
+        int total = num + 5;
+        return total;
+    }
+}
