ff658a3f3e4e865b99dfe07fa175ab39539d5d51
Hêlēl <34353323+daminfer@users.noreply.github.com>
2022-06-22 23:51:27 +0200

    Add files via upload

---
+++ Python/Find_the_capitals.py
@@ -0,0 +1,13 @@
+# Problème Rank: 7 Kyu
+
+"""
+Instructions:
+Write a function that takes a single string (word) as argument. The function must return an ordered list containing the indexes of all capital letters in the string.
+
+Example:
+Test.assertSimilar( capitals('CodEWaRs'), [0,3,4,6] );
+
+"""
+
+def capitals(word):
+    return [i for (i, c) in enumerate(word) if c.isupper()]
