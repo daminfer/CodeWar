dd6c69e830b20baad8c41b1997d338b9eac22d44
Hêlēl <34353323+daminfer@users.noreply.github.com>
2022-06-25 23:38:44 +0200

    Add files via upload

---
+++ Python/Sauterelle_Messi_Goals.py
@@ -0,0 +1,26 @@
+# Problème Rank: 8 Kyu
+
+"""
+Total de buts de Messi
+Utilisez des variables pour trouver la somme des buts marqués par Messi dans 3 compétitions
+
+Informations
+Statistiques de buts de Messi :
+
+Concurrence	Buts
+la Ligue	43
+Ligue des champions	dix
+Coupe du Roi	5
+Tâche
+Créez ces trois variables et stockez les valeurs appropriées à l'aide du tableau ci-dessus :
+la_liga_goals
+champions_league_goals
+copa_del_rey_goals
+Créez une quatrième variable nommée total_goalsqui stocke la somme de tous les objectifs de Messi pour cette année.
+"""
+
+la_liga_goals = 43
+champions_league_goals = 10
+copa_del_rey_goals = 5
+
+total_goals = la_liga_goals + champions_league_goals + copa_del_rey_goals
