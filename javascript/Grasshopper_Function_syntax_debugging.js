179918378e03fe4133c6c7d704a770d2b0b43a2c
Hêlēl <34353323+daminfer@users.noreply.github.com>
2022-07-13 23:35:24 +0200

    Add files via upload

---
+++ javascript/Grasshopper_Function_syntax_debugging.js
@@ -0,0 +1,10 @@
+/* Problème Rank: 8 Kyu
+
+Grasshopper - Function syntax debugging
+A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them.
+
+*/
+
+function main (verb, noun) {
+    return verb + noun
+}
