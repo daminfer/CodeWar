3f53cd96d6eebb60bc4269f04ab501cb9cf3b686
Hêlēl <34353323+daminfer@users.noreply.github.com>
2022-07-20 09:22:24 +0200

    Add files via upload

---
+++ Python/get_character_from_ASCII_Value.py
@@ -0,0 +1,20 @@
+# Problème Rank: 8 Kyu
+
+"""
+
+Write a function get_char() / getChar() which takes a number and 
+returns the corresponding ASCII char for that value.
+
+Example:
+
+get_char(65)
+should return:
+'A'
+
+For ASCII table, you can refer to http://www.asciitable.com/
+
+"""
+
+def get_char(value):
+    '''Return the ASCII char for a given value'''
+    return chr(value)
