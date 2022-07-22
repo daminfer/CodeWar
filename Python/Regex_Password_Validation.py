09a54a53422d51ee3c2ed2ac96ecc1d176ea6f48
Hêlēl <34353323+daminfer@users.noreply.github.com>
2022-07-22 12:21:59 +0200

    Add files via upload

---
+++ Python/Regex_Password_Validation.py
@@ -0,0 +1,34 @@
+# Problème Rank: 5 Kyu
+
+# Problème a Réetudier -> Les regex sont importante et utile !
+"""
+You need to write regex that will validate a password to make sure it meets the following criteria:
+
+At least six characters long
+contains a lowercase letter
+contains an uppercase letter
+contains a digit
+only contains alphanumeric characters (note that '_' is not alphanumeric)
+"""
+
+from re import compile, VERBOSE
+
+regex = compile("""
+^              # begin word
+(?=.*?[a-z])   # at least one lowercase letter
+(?=.*?[A-Z])   # at least one uppercase letter
+(?=.*?[0-9])   # at least one number
+[A-Za-z\d]     # only alphanumeric
+{6,}           # at least 6 characters long
+$              # end word
+""", VERBOSE)
+
+regex_2 = (
+    '^'            # start line
+    '(?=.*\d)'     # must contain one digit from 0-9
+    '(?=.*[a-z])'  # must contain one lowercase characters
+    '(?=.*[A-Z])'  # must contain one uppercase characters
+    '[a-zA-Z\d]'   # permitted characters (alphanumeric only)
+    '{6,}'         # length at least 6 chars
+    '$'            # end line
+)
