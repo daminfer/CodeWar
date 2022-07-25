309a486d0133995870d563e11c2d71affc241ebb
Hêlēl <34353323+daminfer@users.noreply.github.com>
2022-07-25 21:59:01 +0200

    Add files via upload

---
+++ Langage C/How_many_lightsabers_do_you_own.c
@@ -0,0 +1,20 @@
+/* Problème Rank: 8 Kyu
+
+Inspired by the development team at Vooza, write the function that
+
+accepts the name of a programmer, and
+returns the number of lightsabers owned by that person.
+The only person who owns lightsabers is Zach, by the way. He owns 18, which is an awesome number of lightsabers. Anyone else owns 0.
+
+For example(Input --> Output):
+
+"anyone else" --> 0
+"Zach" --> 18
+
+*/
+
+#include <string.h>
+
+int HowManyLightsabersDoYouOwn(const char *name) {
+    return strcmp(name, "Zach") == 0 ? 18 : 0;
+}
