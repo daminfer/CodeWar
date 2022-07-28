307b99bf03e3d493979d0ee07b59b1d466813295
Hêlēl <34353323+daminfer@users.noreply.github.com>
2022-07-28 23:51:51 +0200

    Add files via upload

---
+++ Langage C/Automorphic_Number.c
@@ -0,0 +1,49 @@
+/* Problème Rank: 7 Kyu
+
+Definition
+A number is called Automorphic number if and only if its square ends in the same digits as the number itself.
+
+Task
+Given a number determine if it Automorphic or not .
+
+Warm-up (Highly recommended)
+Playing With Numbers Series
+
+Notes:
+
+The number passed to the function is positive
+Input >> Output Examples
+autoMorphic (25) -->> return "Automorphic" 
+
+Explanation:
+25 squared is 625 , Ends with the same number's digits which are 25 .
+autoMorphic (13) -->> return "Not!!"
+
+Explanation:
+13 squared is 169 , Not ending with the same number's digits which are 69 .
+autoMorphic (76) -->> return "Automorphic"
+
+Explanation:
+76 squared is 5776 , Ends with the same number's digits which are 76 .
+autoMorphic (225) -->> return "Not!!"
+
+Explanation:
+225 squared is 50625 , Not ending with the same number's digits which are 225 .
+autoMorphic (625) -->> return "Automorphic"
+
+Explanation:
+625 squared is 390625 , Ends with the same number's digits which are 625 .
+autoMorphic (1) -->> return "Automorphic"
+
+Explanation:
+1 squared is 1 , Ends with the same number's digits which are 1 .
+autoMorphic (6) -->> return "Automorphic"
+
+Explanation:
+6 squared is 36 , Ends with the same number's digits which are 6
+
+*/
+
+const char *autoMorphic(int n){
+    return n*n % (int)pow(10,(int)log10(n)+1) == n ? "Automorphic" : "Not!!";
+}
