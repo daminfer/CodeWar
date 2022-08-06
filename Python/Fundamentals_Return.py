122438cf467806c986f7d4b7ddcb6fac1f0f008c
Hêlēl <34353323+daminfer@users.noreply.github.com>
2022-08-06 11:54:35 +0200

    Add files via upload

---
+++ Python/Fundamentals_Return.py
@@ -0,0 +1,33 @@
+# Problème Rank: 8 Kyu
+
+"""
+Make multiple functions that will return the sum, 
+difference, modulus, product, quotient, and the exponent respectively.
+
+Please use the following function names:
+
+addition = add
+
+multiply = multiply
+
+division = divide (both integer and float divisions are accepted)
+
+modulus = mod
+
+exponential = exponent
+
+subtraction = subt
+
+Note: All math operations will be: a (operation) b
+
+"""
+
+add = lambda a, b: a + b
+multiply = lambda a, b: a * b
+divide = lambda a, b: a / b
+mod = lambda a, b: a % b
+exponent = lambda a, b: a ** b
+subt = lambda a, b:a - b
+# Make more functions. Refer to the description for function names.
+# The code will NOT WORK IF YOU USE names other than the ones
+# from the description
