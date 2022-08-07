30b38af32cb05bf354e27b8396c8e0bc22313cfa
Hêlēl <34353323+daminfer@users.noreply.github.com>
2022-08-07 23:28:32 +0200

    Add files via upload

---
+++ Python/Surface_Area_and_Volume_of_a_Box.py
@@ -0,0 +1,11 @@
+# Problème Rank: 8 Kyu
+
+"""
+Write a function that returns the total surface area and volume of a box as an array: [area, volume]
+
+"""
+
+def get_size(w, h, d):
+    area = 2*(w*h + h*d + w*d)
+    volume = w*h*d
+    return [area, volume]
