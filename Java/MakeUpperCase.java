5258d42fc371f77afa832e27384ca83baaa34c70
Hêlēl <34353323+daminfer@users.noreply.github.com>
2022-09-24 08:50:47 +0200

    Add files via upload

---
+++ Java/MakeUpperCase.java
@@ -0,0 +1,11 @@
+/* Problème Rank: 8 Kyu
+
+Write a function which converts the input string to uppercase.
+
+*/
+
+public class MakeUpperCase {
+    public static String MakeUpperCase(String str){
+        return str.toUpperCase();
+    }
+}
