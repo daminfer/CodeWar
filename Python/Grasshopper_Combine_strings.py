aedd417b1e5fa87b829f8ffefbd1c6d0f4f27201
Hêlēl <34353323+daminfer@users.noreply.github.com>
2022-09-27 17:36:02 +0200

    Add files via upload

---
+++ Python/Grasshopper_Combine_strings.py
@@ -0,0 +1,19 @@
+# Problème Rank: 8 Kyu
+
+"""
+
+Combine strings function
+Create a function named (combine_names) that accepts two parameters (first and last name). 
+The function should return the full name.
+
+Example:
+
+combine_names('James', 'Stevens')
+returns:
+
+'James Stevens'
+
+"""
+
+def combine_names(mot1, mot2):
+    return mot1 + ' ' + mot2
