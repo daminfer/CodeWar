95f689a211c2ba1e44fa940c360051b2764f9dc5
Hêlēl <34353323+daminfer@users.noreply.github.com>
2022-10-20 09:17:43 +0200

    Add files via upload

---
+++ Ocaml/Composing_squared_strings.ml
@@ -0,0 +1,39 @@
+(* Problème Rank: 7 Kyu *)
+
+(*
+   
+A squared string is a string of n lines, each substring being n characters long. 
+We are given two n-squared strings. For example:
+
+s1 = "abcd\nefgh\nijkl\nmnop" s2 = "qrst\nuvwx\nyz12\n3456"
+
+Let us build a new string strng of size (n + 1) x n in the following way:
+
+The first line of strng has the first char of the first line of s1 plus the chars of the last line of s2.
+The second line of strng has the first two chars of the second line of s1 plus the chars of the penultimate 
+line of s2 except the last char
+
+and so on until the nth line of strng has the n chars of the nth line of s1 plus 
+the first char of the first line of s2.
+
+Calling this function compose(s1, s2) we have:
+
+compose(s1, s2) -> "a3456\nefyz1\nijkuv\nmnopq"
+or printed:
+abcd    qrst  -->  a3456
+efgh    uvwx       efyz1
+ijkl    yz12       ijkuv
+mnop    3456       mnopq
+
+*)
+
+let compose(s1: string) (s2: string): string =
+  let arr1 = Str.split(Str.regexp "\n") s1 |> Array.of_list in
+  let arr2 = Str.split(Str.regexp "\n") s2 |> Array.of_list in
+    rev_inplace_ arr2;
+    let l = Array.length arr2 in
+    let res = Array.make l "" in
+      for i = 0 to l-1 do
+        res.(i) <- String.sub arr1.(i) 0 (i + 1) ^ String.sub arr2.(i) 0 (l - i);
+      done;
+      res |> Array.to_list |> String.concat "\n";;
