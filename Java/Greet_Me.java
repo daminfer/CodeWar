50d3ae3e20a3c6072cfe29b460af13108d81d0f7
Hêlēl <34353323+daminfer@users.noreply.github.com>
2022-10-25 13:24:06 +0200

    Add files via upload

---
+++ Java/Greet_Me.java
@@ -0,0 +1,16 @@
+/* Problème Rank: 7 Kyu
+
+Write a method that takes one argument as name and then greets that name, capitalized and ends with an exclamation point.
+
+Example:
+
+"riley" --> "Hello Riley!"
+"JACK"  --> "Hello Jack!"
+
+*/
+
+public class Greet_Me {
+    public static String greet(String name){
+        return "Hello " + name.substring(0,1).toUpperCase() + name.substring(1).toLowerCase() + "!";
+    }    
+}
