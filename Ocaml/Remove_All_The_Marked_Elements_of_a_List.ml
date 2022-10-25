0f60207125ede0188c80cded18881dd1e10a61bb
Hêlēl <34353323+daminfer@users.noreply.github.com>
2022-10-25 13:08:10 +0200

    Add files via upload

---
+++ Ocaml/Remove_All_The_Marked_Elements_of_a_List.ml
@@ -0,0 +1,16 @@
+(* Problème Rank: 7 Kyu *)
+
+(*
+
+Define a method/function that removes from a given array of integers all the values contained in a second array.
+
+Examples (input -> output):
+* [1, 1, 2, 3, 1, 2, 3, 4], [1, 3] -> [2, 2, 4]
+* [1, 1, 2, 3, 1, 2, 3, 4, 4, 3, 5, 6, 7, 2, 8], [1, 3, 4, 2] -> [5, 6, 7, 8]
+* [8, 2, 7, 2, 3, 4, 6, 5, 4, 4, 1, 2, 3], [2, 4, 3] -> [8, 7, 6, 5, 1]
+
+*)
+
+let remove (integer_list: int list) (values_list: int list): int list =
+  let not x = List.for_all ((<>) x) values_list in 
+  List.filter not integer_list
