278ad03f6b0e7f65085f678f783cd51fc68904a8
Hêlēl <34353323+daminfer@users.noreply.github.com>
2022-10-26 23:16:50 +0200

    Add files via upload

---
+++ Java/Return_the_day.java
@@ -0,0 +1,29 @@
+/* Problème Rank: 8 Kyu
+
+Complete the function which returns the weekday according to the input number:
+
+1 returns "Sunday"
+2 returns "Monday"
+3 returns "Tuesday"
+4 returns "Wednesday"
+5 returns "Thursday"
+6 returns "Friday"
+7 returns "Saturday"
+Otherwise returns "Wrong, please enter a number between 1 and 7"
+
+*/
+
+public class Return_the_day {
+    public static String getDay(int n) {
+        return switch(n){
+            case 1 -> "Sunday";
+            case 2 -> "Monday";
+            case 3 -> "Tuesday";
+            case 4 -> "Wednesday";
+            case 5 -> "Thursday";
+            case 6 -> "Friday";
+            case 7 -> "Saturday";
+            default -> "Wrong, please enter a number between 1 and 7";
+        };
+    }
+}
